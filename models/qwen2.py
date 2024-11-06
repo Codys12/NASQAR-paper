5c194f849540d73fd308409e1bc4f089f93f6d32
SmerkyG <SmerkyG@users.noreply.github.com>
2024-11-06 16:37:05 -0500

    silu gate lora

--- models/qwen2.py
+++ models/qwen2.py
@@ -292,10 +292,13 @@ class TMix_qwen2rwkv(TMix_qwen2):
             #     tmp[n] = ratio_0_to_1 * (1 - (n / (dim_att - 1))) + zigzag
             # self.time_faaaa = nn.Parameter(tmp.reshape(self.n_head, self.head_size))
 
-        self.gate = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias=True)
+        #self.gate = nn.Linear(self.hidden_size, self.num_heads * self.head_dim, bias=True)
         # start gate out with no effect
-        nn.init.zeros_(self.gate.weight)
-        nn.init.ones_(self.gate.bias)
+        #nn.init.zeros_(self.gate.weight)
+        #nn.init.ones_(self.gate.bias)
+        D_GATE_LORA = 128
+        self.gate_w1 = nn.Parameter(torch.zeros(n_embd, D_GATE_LORA)) #nn.Parameter(ortho_init(torch.zeros(args.n_embd, D_GATE_LORA), 0.1))
+        self.gate_w2 = nn.Parameter(torch.zeros(D_GATE_LORA, dim_att).uniform_(-0.01, 0.01)) #nn.Parameter(ortho_init(torch.zeros(D_GATE_LORA, dim_att), 0.1))
 
         #self.ln_x = nn.LayerNorm(dim_att)
 
@@ -327,7 +330,8 @@ class TMix_qwen2rwkv(TMix_qwen2):
         value_states = self.v_proj(xv)
         #decay_states = self.time_decay.view(1,1,H,1).expand(bsz,q_len,H,1).contiguous()
         decay_states = (self.time_decay + torch.tanh(xw @ self.time_decay_w1) @ self.time_decay_w2).to(query_states.dtype)
-        g = F.silu(self.gate(xg))
+        #g = F.silu(self.gate(xg))
+        g = F.sigmoid(torch.tanh(xg @ self.gate_w1) @ self.gate_w2)
 
         query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
         key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
