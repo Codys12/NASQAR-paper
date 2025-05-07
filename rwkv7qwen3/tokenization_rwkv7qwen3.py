cba2252d221d5fed6697be2a6fb754c38d18455c
SmerkyG <SmerkyG@users.noreply.github.com>
2025-05-06 21:33:02 -0400

    removed unused

--- rwkv7qwen3/tokenization_rwkv7qwen3.py
+++
@@ -1,4 +0,0 @@
-from transformers.models.qwen3.tokenization_qwen3 import Qwen3Tokenizer
-
-class RWKV6Qwen3Tokenizer(Qwen3Tokenizer):
-    pass
--- rwkv7qwen3/tokenization_rwkv7qwen3_fast.py
+++
@@ -1,4 +0,0 @@
-from transformers.models.qwen2.tokenization_qwen3_fast import Qwen3TokenizerFast
-
-class RWKV6Qwen3TokenizerFast(Qwen3TokenizerFast):
-    pass
